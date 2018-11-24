6606b50d5daafc3001207dd639786d93bd785ce3
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-24 01:02:28 -0600

    Se ingresa Email Contact1.5

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var express         = require("express"),
     Contacto        = require("./models/contacto"),
     nodemailer      = require("nodemailer");
 
-mongoose.connect("mongodb://localhost/Descubramos", {useNewUrlParser: true});    
-//mongoose.connect("mongodb://jorge:jorge007@ds141960.mlab.com:41960/descubramos", {useNewUrlParser: true});
+//mongoose.connect("mongodb://localhost/Descubramos", {useNewUrlParser: true});    
+mongoose.connect("mongodb://jorge:jorge007@ds141960.mlab.com:41960/descubramos", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
