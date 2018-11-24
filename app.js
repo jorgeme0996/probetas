f02bdb8404c563544fb04613065eb929248280a4
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-24 00:41:08 -0600

    Se ingresa Email Contact

--- app.js
+++ app.js
@@ -29,6 +29,49 @@ app.post("/contacto", function(req, res){
         if(err){
             console.log(err);
         } else {
+            const output = `
+            <p> Tienes que contactar a alguien </p>
+            <h3> Detalles del contacto </h3>
+            <ul>
+                <li>Nombre: ${newContacto.name}</li>
+                <li>Apellido: ${newContacto.lastName}</li>
+                <li>Email: ${newContacto.email}</li>
+                <li>Telefono de contacto: ${newContacto.celular}</li>
+            </ul>
+            `;
+
+            let transporter = nodemailer.createTransport({
+                host: 'mail.privateemail.com',
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                    user: 'admin@descubramos.mx', // generated ethereal user
+                    pass: 'J0rg3m30996' // generated ethereal password
+                },
+            });
+
+            // setup email data with unicode symbols
+            let mailOptions = {
+                from: '"Nodemailer Contact" <contacto@descubramos.mx>', // sender address
+                to: 'jorgeme0996@gmail.com', // list of receivers
+                subject: 'Requerimiento de Contacto', // Subject line
+                text: 'Prueba', // plain text body
+                html: output // html body
+            };
+
+            // send mail with defined transport object
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    return console.log(error);
+                }
+                console.log('Message sent: %s', info.messageId);
+                // Preview only available when sending through an Ethereal account
+                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+                // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+            });
+
             res.render("programa/correcto.ejs")
         }
     });
