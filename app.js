53d08aa83eccfd4a1b9ec99c5c47ada3c8452ac8
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-23 21:38:24 -0600

    Ya la ultima

---
+++ app.js
@@ -0,0 +1,40 @@
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    Contacto        =require("./models/contacto")
+
+//mongoose.connect("mongodb://localhost/Descubramos", {useNewUrlParser: true});    
+mongoose.connect("mongodb://jorge:jorge007@ds141960.mlab.com:41960/descubramos", {useNewUrlParser: true});
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+app.get("/nosotros", function(req, res){
+    res.render("nosotros/nosotros.ejs");
+});
+
+app.get("/programa", function(req, res){
+    res.render("programa/programa.ejs");
+});
+
+app.post("/contacto", function(req, res){
+    var newContacto = req.body.contacto;
+    Contacto.create(newContacto, function(err, newlyContacto){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("programa/correcto.ejs")
+        }
+    });
+
+});
+
+app.listen(process.env.PORT,process.env.IP, function(){
+    console.log("Esta vivo!!!!")
+})
+//process.env.PORT,process.env.IP
---
+++ models/contacto.js
@@ -0,0 +1,10 @@
+var mongoose = require("mongoose");
+
+var ContactoSchema = new mongoose.Schema({
+    name: String,
+    lastName: String,
+    email: String,
+    celular: String
+});
+
+module.exports = mongoose.model("Contacto", ContactoSchema);
