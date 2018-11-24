1c3dce011c5a21c9fd55ad10260d471e82dd80e4
jorgeme0996 <jorgeme0996@gmail.com>
2018-11-24 00:55:03 -0600

    Se ingresa Email Contact1.3

--- app.js
+++ app.js
@@ -42,15 +42,12 @@ app.post("/contacto", function(req, res){
 
             let transporter = nodemailer.createTransport({
                 host: 'mail.privateemail.com',
-                port: 587,
-                secure: false, // true for 465, false for other ports
+                port: 465,
+                secure: true, // true for 465, false for other ports
                 auth: {
                     user: 'admin@descubramos.mx', // generated ethereal user
                     pass: 'J0rg3m30996' // generated ethereal password
                 },
-                tls:{
-                    rejectUnauthorized: false
-                }
             });
 
             // setup email data with unicode symbols
